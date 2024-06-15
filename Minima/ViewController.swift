e17ea45f44c6c49051ee93d40e6cd117d1d49479
Jaka <jaka0926@naver.com>
2024-06-15 16:34:54 +0900

    Initial Commit

---
+++ Minima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minima
+//
+//  Created by Jaka on 2024-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
